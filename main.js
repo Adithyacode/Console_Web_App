082fa1d669f8546ea4c7b6bc9297d469f6f31bdd
Adithyacode <86084445+Adithyacode@users.noreply.github.com>
2021-09-20 20:03:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+x = console.log("Adithya");
+console.log(x);
+y = console.log("Veeraraghavan");
+console.log(y);
+z = x.concat(y);
+console.log(z);
+a = 10;
+b = 20;
+add = a+b;
+console.log(add);
+r = 1098;
+n = 3789;
+subtract = n-r;
+console.log(subtract);
+t = 103;
+d = 202;
+multiply = d*t;
+console.log(multiply);
+g = 15;
+s = 34;
+divide = a/b;
+console.log(divide);
+
+<! % = modulus operator >
